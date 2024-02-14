ff2f872ef7e2226027d14cb6a2fe315633db4a47
DavidCabrerizo <visualproductor@gmail.com>
2024-02-14 10:34:15 +0100

    quinto video mitad

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+//importamos lso componentes
+
+import CompShowBlogs from './blog/ShowBlogs';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <button className='btn btn-primary'><i className="fa-brands fa-react"></i></button>
+       
       </header>
+      <CompShowBlogs></CompShowBlogs>
     </div>
   );
 }
